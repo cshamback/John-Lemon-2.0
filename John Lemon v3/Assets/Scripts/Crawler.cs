5deed1581a588b23c64545e82b6a82bea4c109cb
WilliamStarling <williamjonas@comcast.net>
2024-10-27 19:56:25 -0500

    Added navigation Mesh
    Added navigation for crawler enemy so that it can move towards the player.

--- John Lemon v3/Assets/Scripts/Crawler.cs
+++ John Lemon v3/Assets/Scripts/Crawler.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))] //Requires there to be a navmesh agent component.
 public class Crawler : MonoBehaviour
 {
+    [SerializeField] private Transform target; //What the agent will move towards.
+    private NavMeshAgent agent;
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        agent.destination = target.position;
     }
 }
